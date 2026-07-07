117b9b143623754457ee9669b6cdf4046044989f
Dirie <dirieisse0@gmail.com>
2026-07-07 15:46:54 +0100

    feat: adding s3 resources for bootstrap

---
+++ app/terraform/bootstrap/modules/s3/s3.tf
@@ -0,0 +1,34 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_name
+  region = var.region
+     
+  lifecycle {
+    prevent_destroy = true
+  }
+  tags = var.tags
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+    bucket = aws_s3_bucket.terraform_state.id
+
+    versioning_configuration {
+      status = "Enabled"
+    }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bootstrap_s3_bucket_acl" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}

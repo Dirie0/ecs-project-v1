1563d87dfca0ba0ce1e7e866f46a77401588b20e
Dirie <dirieisse0@gmail.com>
2026-07-07 21:46:47 +0100

    feat: attaching ecstaskexecution role policy to ecs task execution role and also creating ecs task role

--- app/terraform/modules/iam/main.tf
+++ app/terraform/modules/iam/main.tf
@@ -21,3 +21,22 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   )
 }
 
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
+resource "aws_iam_role" "ecs_task_role" {
+  name               = "${var.environment}-ecs-task-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_trust.json
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.environment}-ecs-task-role"
+      Service = "iam"
+    }
+  )
+}
+

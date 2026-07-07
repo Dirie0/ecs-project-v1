87fa60c20c6a419703f3c5b5976b5afa75cfcc9e
Dirie <dirieisse0@gmail.com>
2026-07-07 19:51:30 +0100

    feat: attaching public subnets to public route table that internet access

--- app/terraform/modules/vpc/main.tf
+++ app/terraform/modules/vpc/main.tf
@@ -98,5 +98,13 @@ resource "aws_route_table" "public" {
   )
 }
 
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+
 
 

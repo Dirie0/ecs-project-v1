783383b26bf62bde591cfcc55bd69765045427cd
Dirie <dirieisse0@gmail.com>
2026-07-12 14:43:31 +0100

    feat: simplifying acm certificate validation using for each approach instead of explicit indexing

--- terraform/modules/acm/main.tf
+++ terraform/modules/acm/main.tf
@@ -1,43 +1,33 @@
 resource "aws_acm_certificate" "app" {
-
   domain_name       = var.domain_name
   validation_method = "DNS"
+
   lifecycle {
     create_before_destroy = true
   }
-
 }
-
 
 resource "aws_route53_record" "validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.app.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-  zone_id = var.zone_id
-
-  name = tolist(
-    aws_acm_certificate.app.domain_validation_options
-  )[0].resource_record_name
-
-  type = tolist(
-    aws_acm_certificate.app.domain_validation_options
-  )[0].resource_record_type
-
-  records = [
-    tolist(
-      aws_acm_certificate.app.domain_validation_options
-    )[0].resource_record_value
-  ]
-
-  ttl = 60
-
+  zone_id         = var.zone_id
+  name            = each.value.name
+  type            = each.value.type
+  records         = [each.value.record]
+  ttl             = 60
+  allow_overwrite = true
 }
 
-
 resource "aws_acm_certificate_validation" "app" {
-
   certificate_arn = aws_acm_certificate.app.arn
 
   validation_record_fqdns = [
-    aws_route53_record.validation.fqdn
+    for record in aws_route53_record.validation : record.fqdn
   ]
-
 }
